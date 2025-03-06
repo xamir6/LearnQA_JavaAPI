d90cd9a9022d091f2d26e59044e4066513037ca5
perokhin <perokhin@sberhealt.ru>
2025-03-06 18:50:28 +0300

    helloWorldTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,18 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
-
     @Test
     public void helloWorldTest() {
-        System.out.println("Hello world!");
+        System.out.println("Hello from Pavel");
+    }
+
+    @Test
+    public void helloWorldTest1() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }
