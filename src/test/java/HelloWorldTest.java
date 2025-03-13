7a4d5ebf70a06272e9b7f505e7d6213e4cedbf4e
perokhin <perokhin@sberhealt.ru>
2025-03-13 17:22:19 +0300

    added longRedirectTest()

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,5 +1,8 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
@@ -12,5 +15,20 @@ public class HelloWorldTest {
 
         String answer = response.get("messages[1].message");
         System.out.println(answer);
+    }
+
+    @Test
+    public void longRedirectTest() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(true)
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        Headers responceHeaders = response.getHeaders();
+        Header siteHeader = responceHeaders.get("x-host");
+        String value = siteHeader.getValue();
+        System.out.println(value);
     }
 }
