e3811c7aaf4f70be205a4ed23ed80a960f3e3cea
perokhin <perokhin@sberhealt.ru>
2025-03-23 14:56:16 +0300

    userAgentTest()

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,17 +4,19 @@ import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldTest {
     @Test
     public void jsonParsingTest() {
-        JsonPath response = RestAssured
-                .given()
+        JsonPath response = given()
                 .get("https://playground.learnqa.ru/api/get_json_homework")
                 .jsonPath();
 
@@ -24,8 +26,7 @@ public class HelloWorldTest {
 
     @Test
     public void longRedirectTest() {
-        Response response = RestAssured
-                .given()
+        Response response = given()
                 .redirects()
                 .follow(false)
                 .get("https://playground.learnqa.ru/api/long_redirect")
@@ -42,8 +43,7 @@ public class HelloWorldTest {
         String currentUrl = "https://playground.learnqa.ru/api/long_redirect";
 
         while (true) {
-            Response response = RestAssured
-                    .given()
+            Response response = given()
                     .redirects()
                     .follow(false)
                     .get(currentUrl)
@@ -61,24 +61,21 @@ public class HelloWorldTest {
     public void longtimeJobTest() throws InterruptedException {
         String url = "https://playground.learnqa.ru/ajax/api/longtime_job";
 
-        JsonPath response = RestAssured
-                .given()
+        JsonPath response = given()
                 .get(url)
                 .jsonPath();
 
         String tokenValue = response.get("token");
         int secondsValue = response.get("seconds");
 
-        JsonPath responseBeforeTaskReady = RestAssured
-                .given()
+        JsonPath responseBeforeTaskReady = given()
                 .queryParam("token", tokenValue)
                 .get(url)
                 .jsonPath();
 
         if ("Job is NOT ready".equals(responseBeforeTaskReady.get("status"))) {
             Thread.sleep((secondsValue + 1) * 1000L);
-            JsonPath responseAfterTaskReady = RestAssured
-                    .given()
+            JsonPath responseAfterTaskReady = given()
                     .queryParam("token", tokenValue)
                     .get(url)
                     .jsonPath();
@@ -101,8 +98,7 @@ public class HelloWorldTest {
         );
 
         for (String pass : passwords) {
-            Response response = RestAssured
-                    .given()
+            Response response = given()
                     .queryParam("login", login)
                     .queryParam("password", pass)
                     .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
@@ -110,8 +106,7 @@ public class HelloWorldTest {
 
             String responseCookie = response.getCookie("auth_cookie");
 
-            Response checkResponse = RestAssured
-                    .given()
+            Response checkResponse = given()
                     .cookie("auth_cookie", responseCookie)
                     .get("https://playground.learnqa.ru/ajax/api/check_auth_cookie")
                     .andReturn();
@@ -159,6 +154,56 @@ public class HelloWorldTest {
         String header = response.getHeader("x-secret-homework-header");
         assertEquals("Some secret value", header);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+            "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
+    })
+
+    public void userAgentTest(String userAgent) {
+        Response response = given()
+                .header("User-Agent", userAgent) // Передаем User-Agent как заголовок
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check");
+        response.prettyPrint();
+
+        String actualPlatform = response.jsonPath().getString("platform");
+        String actualBrowser = response.jsonPath().getString("browser");
+        String actualDevice = response.jsonPath().getString("device");
+
+        switch (userAgent) {
+            case "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30":
+                assertEquals("Mobile", actualPlatform);
+                assertEquals("No", actualBrowser);
+                assertEquals("Android", actualDevice);
+                break;
+
+            case "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1":
+                assertEquals("Mobile", actualPlatform);
+                assertEquals("Chrome", actualBrowser);
+                assertEquals("iOS", actualDevice);
+                break;
+
+            case "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)":
+                assertEquals("Googlebot", actualPlatform);
+                assertEquals("Unknown", actualBrowser);
+                assertEquals("Unknown", actualDevice);
+                break;
+
+            case "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0":
+                assertEquals("Web", actualPlatform);
+                assertEquals("Chrome", actualBrowser);
+                assertEquals("No", actualDevice);
+                break;
+
+            case "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1":
+                assertEquals("Mobile", actualPlatform);
+                assertEquals("No", actualBrowser);
+                assertEquals("iPhone", actualDevice);
+                break;
+        }
+    }
 }
-
-
